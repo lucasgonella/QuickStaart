d51688c508806b47506645c63454e42780e68641
Lucas Gonella <goonella@hotmail.com>
2022-07-30 17:43:57 -0300

    att

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
 function revelarResposta() {
-    var resposta = document.querySelector('#resposta');
-    resposta.classList.toggle('blur');
+	var resposta = document.querySelector('#resposta');
+	resposta.classList.toggle('blur');
 }
 
 function proximaPergunta(proximaPergunta) {
-    var card = document.querySelector('#cardContainer');
-    card.innerHTML = ""
+	var card = document.querySelector('#cardContainer');
+	card.innerHTML = ""
 
-    var novaDiv = document.createElement('div');
+	var novaDiv = document.createElement('div');
 
-    novaDiv.classList.add('card', "animate__animated", "animate__backInRight");
+	novaDiv.classList.add('card', "animate__animated", "animate__backInRight");
 
-    novaDiv.innerHTML = `
-        <div class="card-pergunta centralizar">
-            <h1 class="pergunta">O que é ${proximaPergunta.title}?</h1>
-        </div>    
+	novaDiv.innerHTML = `
+		<div class="card-pergunta centralizar">
+			<h1 class="pergunta">O que é ${proximaPergunta.title}?</h1>
+		</div>    
 
-        <div id="resposta" class="card-resposta blur">
-            <p class="resposta">${proximaPergunta.description}</p>
-        </div>
-        `;
+		<div id="resposta" class="card-resposta blur">
+			<p class="resposta">${proximaPergunta.description}</p>
+		</div>
+		`;
 
-    card.appendChild(novaDiv)
+	card.appendChild(novaDiv)
 
 };
 
 function buscarInformacao() {
-    fetch("https://flash.quickstaart.com/random")
-    .then(function (resultado) {
-        return resultado.json();
-    })
-    .then(function (resultadoJson) {
-        proximaPergunta(resultadoJson);
-    });
-}~
+  fetch("https://flash.quickstaart.com/random")
+	.then(function (resultado) {
+	  return resultado.json();
+	})
+	.then(function (resultadoJson) {
+	  proximaPergunta(resultadoJson);
+	});
+}
 
 window.addEventListener("load", buscarInformacao);
